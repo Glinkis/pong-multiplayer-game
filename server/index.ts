1570567980f8aa79dbc70ece7f77cdbe0d6b9b98
Victor Glindås <glinkis@gmail.com>
2019-05-08 21:35:31 +0200

    Format server logging

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,11 @@
 import socketIo from "socket.io";
+import { green, blue } from "ansi-colors";
 import { Events } from "../shared/events";
 
 const io = socketIo();
 
 io.on("connection", socket => {
-  console.log(`User ${socket.id} connected!`);
+  console.log(blue(`User ${socket.id} connected!`));
 
   for (const [key, value] of Object.entries(io.sockets.sockets)) {
     if (socket.id !== key) {
@@ -13,9 +14,12 @@ io.on("connection", socket => {
   }
 
   socket.on("disconnect", () => {
-    console.log(`User ${socket.id} disconnected!`);
+    console.log(blue(`User ${socket.id} disconnected!`));
     io.emit(Events.UserDisconnected, socket.id);
   });
 });
 
 io.listen(3030);
+
+console.clear();
+console.log(green("Server is listening on port 3030"));
